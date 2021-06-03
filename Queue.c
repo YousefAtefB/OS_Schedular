0336dc94f77190dc7d438738785e6d1480f384e8
YousefAtefB <YousefAtef.b.a.t@gmail.com>
2021-06-03 21:34:56 +0200

    queue

---
+++ Queue.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node node;
+typedef struct queue queue;
+
+//__________________________________________________
+struct node
+{
+    int data;
+    node *nxt;
+};
+
+void node_init(node* n,int x)
+{
+    n->data=x;
+    n->nxt=NULL;
+}
+
+//__________________________________________________
+
+
+struct queue
+{
+    node *front, *rear;
+};
+
+queue* queue_init()
+{
+    queue *q =malloc(sizeof(queue));
+    q->front=q->rear=NULL;
+    return q;
+}
+
+void queue_push(queue *q,int x)
+{
+    node *temp=malloc(sizeof(node));
+    node_init(temp,x);
+
+    if(q->rear==NULL)
+    {
+        q->front=q->rear=temp;
+        return;
+    }
+
+    q->rear->nxt=temp;
+    q->rear=temp;
+}
+
+void queue_pop(queue *q)
+{
+    node *temp=q->front;
+    q->front=q->front->nxt;
+
+    if(q->front==NULL)
+        q->rear=NULL;
+
+    free(temp);
+}
+
+int queue_front(queue * q)
+{
+    return q->front->data;
+}
+int main()
+{
+    queue *q = queue_init();
+    queue_push(q,1);
+    queue_push(q,44);
+    queue_push(q,12);
+    queue_push(q,71);
+
+    for(int i=0;i<4;i++)
+    {
+        printf("%d\n",queue_front(q));
+        queue_pop(q);
+    }
+
+    free(q);
+
+}
